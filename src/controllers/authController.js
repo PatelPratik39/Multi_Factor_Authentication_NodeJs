255d96cd2c7db1554464b87efdd65c00c36400a1
PatelPratik39 <ptl.pratik717@gmail.com>
2024-10-04 12:13:58 -0500

    QRcode is getting genarated

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.js";
+import speakeasy from "speakeasy";
+import jwt from "jsonwebtoken";
+import qrCode from "qrcode";
 
 export const register = async (req, res) => {
   try {
@@ -57,7 +60,30 @@ export const logout = async (req, res) => {
   });
 };
 
-export const setup2FA = async (req, res) => {};
+export const setup2FA = async (req, res) => {
+  try {
+    console.log("The req.User is : ", req.user);
+    const user = req.user;
+    var secret = speakeasy.generateSecret();
+    console.log("The Secret object is : ", secret);
+    user.twoFactorSecret = secret.base32;
+    user.isMfaActive = true;
+    await user.save();
+    const url = speakeasy.otpauthURL({
+      secret: secret.base32,
+      label: `${req.user.username}`,
+      issuer: "www.pratik's_MERNStack_Projects.com",
+      encoding: "base32"
+    });
+    const qrImageUrl = await qrCode.toDataURL(url);
+    res.status(200).json({
+      secret: secret.base32,
+      qrCode: qrImageUrl
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error settin up MFA", message: error });
+  }
+};
 
 export const verify2FA = async (req, res) => {};
 
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -25,12 +25,33 @@ router.get("/status", authStatus);
 router.post("/logout", logout);
 
 // 2FA Route
-router.post("/2fa/setup", setup2FA);
+router.post(
+  "/2fa/setup",
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: " Unauthorized " });
+  },
+  setup2FA
+);
 
 // verify Route
-router.post("/2fa/verify", verify2FA);
+router.post(
+  "/2fa/verify",
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: " Unauthorized " });
+  },
+  verify2FA
+);
 
 // 2FA Route
-router.post("/2fa/reset", reset2FA);
+router.post(
+  "/2fa/reset",
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.status(401).json({ message: " Unauthorized " });
+  },
+  reset2FA
+);
 
 export default router;
